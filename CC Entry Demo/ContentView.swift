61dad8a47d2583cc620f419c9d9e9a53aac6027e
KMessina1 <KMWeb@Mac.com>
2024-01-12 11:57:25 -0500

    Initial Commit

---
+++ CC Entry Demo/ContentView.swift
@@ -0,0 +1,32 @@
+//
+
+/*--------------------------------------------------------------------------------------------------------------------------
+    File: ContentView.swift
+  Author: Kevin Messina
+ Created: 1/12/24
+Modified:
+ 
+©2024 Creative App Solutions, LLC. - All Rights Reserved.
+----------------------------------------------------------------------------------------------------------------------------
+NOTES:
+--------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
